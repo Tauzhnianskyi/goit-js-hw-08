b8e2a693cfbc763494808cc622bb51773c2cfa1c
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-10-02 13:13:49 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,33 @@
 // Add imports above this line
 import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(simpleLightbox);
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const item = createGalleryItem(galleryItems);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `;
+    })
+    .join('');
+}
+
+gallery.addEventListener('click', onClick);
+gallery.insertAdjacentHTML('beforeend', item);
+
+function onClick(evt) {
+  evt.preventDefault();
+}
+
+let galleryClick = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});

df594ec0b9468078915244bc18ce05a96aeac3c3
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-10-02 23:27:19 +0300

    hw-2 comleted

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,17 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
-const iframe = document.getElementById('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const onPlay = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on('timeupdate', onPlay);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
